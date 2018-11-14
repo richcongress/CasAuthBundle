dc6aab26d1369c78393187af39ea7f73cc7130dc
Antoine Rouault de Coligny <arouaultdecoligny@richcongress.com>
2018-11-14 10:45:09 +0100

    CAS attributes available in user

--- Security/CasAuthenticator.php
+++ Security/CasAuthenticator.php
@@ -23,6 +23,7 @@ class CasAuthenticator extends AbstractGuardAuthenticator
     protected $query_ticket_parameter;
     protected $query_service_parameter;
     protected $options;
+    protected $attributes = array();
 
     private $eventDispatcher;
 
@@ -88,6 +89,7 @@ class CasAuthenticator extends AbstractGuardAuthenticator
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         if (isset($credentials[$this->username_attribute])) {
+            $this->attributes = $this->getCasAttributes($credentials);
             return $userProvider->loadUserByUsername($credentials[$this->username_attribute]);
         } else {
             return null;
@@ -114,6 +116,8 @@ class CasAuthenticator extends AbstractGuardAuthenticator
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
+        $token->setAttributes($this->attributes);
+
         // If authentication was successful, redirect to the current URI with
         // the ticket parameter removed so that it is hidden from end-users.
         if ($request->query->has($this->query_ticket_parameter)) {
@@ -193,5 +197,124 @@ class CasAuthenticator extends AbstractGuardAuthenticator
       $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
       $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
       return "$scheme$user$pass$host$port$path$query$fragment";
+    }
+
+    /**
+     * @param $credentials
+     * @return array
+     */
+    protected function getCasAttributes($credentials) {
+        $attras = array();
+        // "Jasig Style" & CAS 3.0 Attributes:
+        //
+        //   <cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+        //     <cas:authenticationSuccess>
+        //       <cas:user>jsmith</cas:user>
+        //       <cas:attributes>
+        //         <cas:attraStyle>Jasig</cas:attraStyle>
+        //         <cas:surname>Smith</cas:surname>
+        //         <cas:givenName>John</cas:givenName>
+        //         <cas:memberOf>CN=Staff,OU=Groups,DC=example,DC=edu</cas:memberOf>
+        //         <cas:memberOf>CN=Spanish Department,OU=Departments,OU=Groups,DC=example,DC=edu</cas:memberOf>
+        //       </cas:attributes>
+        //       <cas:proxyGrantingTicket>PGTIOU-84678-8a9d2sfa23casd</cas:proxyGrantingTicket>
+        //     </cas:authenticationSuccess>
+        //   </cas:serviceResponse>
+        //
+        if (isset($credentials['attributes'])) {
+            foreach ($credentials['attributes'] as $attribute) {
+
+                if (property_exists($attribute->attributes(), 'name')) {
+                    $name = (string)$attribute->attributes()['name'];
+                } else {
+                    $name = $attribute->getName();
+                }
+
+                if (property_exists($attribute->attributes(), 'value')) {
+                    $value = (string)$attribute->attributes()['value'];
+                } else {
+                    $value = $attribute->__toString();
+                }
+                // Make an attribute multi-valued on the second one seen.
+                if (isset($attras[$name]) && !is_array($attras[$name])) {
+                    $tmp = $attras[$name];
+                    $attras[$name] = array($tmp);
+                }
+                // Add multi-valued attributes.
+                if (isset($attras[$name])) {
+                    $attras[$name][] = $value;
+                }
+                // Single-valued attributes.
+                else {
+                    $attras[$name] = $value;
+                }
+            }
+        } else {
+            // "Name-Value" attributes.
+            //
+            // Attribute format from these mailing list thread:
+            // http://jasig.275507.n4.nabble.com/CAS-attributes-and-how-they-appear-in-the-CAS-response-td264272.html
+            // Note: This is a less widely used format, but in use by at least two institutions.
+            //
+            //   <cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+            //     <cas:authenticationSuccess>
+            //       <cas:user>jsmith</cas:user>
+            //
+            //       <cas:attribute name='attraStyle' value='Name-Value' />
+            //       <cas:attribute name='surname' value='Smith' />
+            //       <cas:attribute name='givenName' value='John' />
+            //       <cas:attribute name='memberOf' value='CN=Staff,OU=Groups,DC=example,DC=edu' />
+            //       <cas:attribute name='memberOf' value='CN=Spanish Department,OU=Departments,OU=Groups,DC=example,DC=edu' />
+            //
+            //       <cas:proxyGrantingTicket>PGTIOU-84678-8a9d2sfa23casd</cas:proxyGrantingTicket>
+            //     </cas:authenticationSuccess>
+            //   </cas:serviceResponse>
+            //
+            if (isset($credentials['attribute']) && isset($credentials['attribute'][0]->attributes()['name']) && isset($credentials['attribute'][0]->attributes()['value'])) {
+                foreach ($credentials['attribute'] as $attribute) {
+                    $name = (string)$attribute->attributes()['name'];
+                    $value = (string)$attribute->attributes()['value'];
+                    // Make an attribute multi-valued on the second one seen.
+                    if (isset($attras[$name]) && !is_array($attras[$name])) {
+                        $tmp = $attras[$name];
+                        $attras[$name] = array($tmp);
+                    }
+                    // Add multi-valued attributes.
+                    if (isset($attras[$name])) {
+                        $attras[$name][] = $value;
+                    }
+                    // Single-valued attributes.
+                    else {
+                        $attras[$name] = $value;
+                    }
+                }
+            }
+            // "RubyCAS Style" attributes
+            //
+            //   <cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+            //     <cas:authenticationSuccess>
+            //       <cas:user>jsmith</cas:user>
+            //
+            //       <cas:attraStyle>RubyCAS</cas:attraStyle>
+            //       <cas:surname>Smith</cas:surname>
+            //       <cas:givenName>John</cas:givenName>
+            //       <cas:memberOf>CN=Staff,OU=Groups,DC=example,DC=edu</cas:memberOf>
+            //       <cas:memberOf>CN=Spanish Department,OU=Departments,OU=Groups,DC=example,DC=edu</cas:memberOf>
+            //
+            //       <cas:proxyGrantingTicket>PGTIOU-84678-8a9d2sfa23casd</cas:proxyGrantingTicket>
+            //     </cas:authenticationSuccess>
+            //   </cas:serviceResponse>
+            //
+            else {
+                // Test for elements other than our allowed list know to the protocol.
+                $skip = array('user', 'proxyGrantingTicket');
+                foreach ($credentials as $name => $value) {
+                    if (!in_array($name, $skip)) {
+                        $attras[$name] = $value;
+                    }
+                }
+            }
+        }
+        return $attras;
     }
 }
